4d58f8eb229a201eed2c6cfa189ff1506a90d028
zekaryas <zekaryasdinku@gmail.com>
2022-12-06 15:17:38 +0300

    - added list page

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -4,26 +4,31 @@ import {HeartIcon, MagnifyingGlassCircleIcon, ShoppingBagIcon} from "@heroicons/
 import logo from '../../public/images/logo.svg';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import {useRouter} from "next/router";
 
-// interface Props {
-//     categories: string[]
-// }
 
 export default function Header() {
-    const categories: string[] = ["smartphones","laptops","fragrances","skincare","groceries","home-decoration","furniture","tops","womens-dresses","womens-shoes","mens-shirts","mens-shoes","mens-watches","womens-watches","womens-bags","womens-jewellery","sunglasses","automotive","motorcycle","lighting"];
-    const mainCategories = categories.slice(0,5);
-    const otherCategories = categories.slice(5, categories.length).sort((a, b) => (a>b ? 1 : 0));
+    const router = useRouter();
+
+    const goTo = (destination: string) => {
+        router.push(`/shop?category=${destination}`);
+    }
+
+    const categories: string[] = ["smartphones", "laptops", "fragrances", "skincare", "groceries", "home-decoration", "furniture", "tops", "womens-dresses", "womens-shoes", "mens-shirts", "mens-shoes", "mens-watches", "womens-watches", "womens-bags", "womens-jewellery", "sunglasses", "automotive", "motorcycle", "lighting"];
+    const mainCategories = categories.slice(0, 5);
+    const otherCategories = categories.slice(5, categories.length).sort((a, b) => (a > b ? 1 : 0));
 
     return <div className="w-full p-3 flex justify-between items-center shadow bg-gray-900 text-white rounded-2xl">
         <Image className="fill-white h-6" src={logo} width={50} height={30} alt="website logo"/>
         <ul className="flex space-x-3 items-center">
             {
                 mainCategories.map((category) => {
-                    return <li key={category}><Link href="components#" className="uppercase">{category}</Link></li>
+                    return <li key={category}><Link href={`/shop?category=${category}`}
+                                                    className="uppercase">{category}</Link></li>
                 })
             }
-            <Dropdown arrowClassName="arrowControl" controlClassName={"control"} options={otherCategories} onChange={() => {
-            }} placeholder="More categories"/>
+            <Dropdown arrowClassName="arrowControl" controlClassName={"control"} options={otherCategories}
+                      onChange={option => goTo(option.value)} placeholder="More categories"/>
         </ul>
         <div className="flex space-x-3">
             <MagnifyingGlassCircleIcon className="h-6 w-6"/>
---
+++ pages/shop/index.tsx
@@ -0,0 +1,33 @@
+import {Product} from "../../models/Product";
+import ProductList from "../../components/Products/ProductList";
+
+interface Props {
+    products: Product[]
+}
+
+function shop({products}: Props) {
+    return <ProductList products={products}/>
+}
+
+export default shop;
+
+export async function getServerSideProps(context: any) {
+
+    const {query} = context;
+    const {category} = query;
+
+    let result = [];
+    if (category) {
+        result = await fetch(`https://dummyjson.com/products/category/${category}`).then(res => res.json());
+    } else {
+        result = await fetch(`https://dummyjson.com/products`).then(res => res.json());
+    }
+
+
+    return {
+        props: {
+            products: result.products
+        }
+    }
+
+}
