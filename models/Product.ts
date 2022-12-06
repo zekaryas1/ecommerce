3f659301777f68704e951ca8f691b940f5fb1ac3
zekaryas <zekaryasdinku@gmail.com>
2022-12-06 13:27:00 +0300

    - defined some models

---
+++ models/Product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: number,
+    title: string,
+    description: string,
+    brand: string,
+    category: string,
+    price: string,
+    rating: string,
+    stock: string,
+    thumbnail: string,
+    images: string[]
+}
