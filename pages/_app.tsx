74edfedcf340d02595a90a9c5cf79b71c9f1cd07
zekaryas <zekaryasdinku@gmail.com>
2022-12-06 13:54:29 +0300

    - created header layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Header from "../components/Layouts/Header";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+        <Header/>
+        <Component {...pageProps} />
+    </>
 }
