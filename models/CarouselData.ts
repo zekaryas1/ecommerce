f5f49c5d80f6f6877e30bf4f156f120ff79d4482
zekaryas <zekaryasdinku@gmail.com>
2022-12-07 17:42:32 +0300

    - updated home page Carousel with better ui

---
+++ models/CarouselData.ts
@@ -0,0 +1,42 @@
+import {StaticImageData} from "next/image";
+import fragrance1 from '../public/images/fragrance-1-removebg-preview.png';
+
+import laptop1 from '../public/images/laptop-1-removebg-preview.png';
+import skincare from '../public/images/skencare-1-removebg-preview.png';
+
+import phone2 from '../public/images/phone-4-removebg-preview.png';
+
+import groceries1 from '../public/images/groceries-1-removebg-preview.png';
+export interface CarouselData {
+    title: string;
+    description: string;
+    image: StaticImageData;
+}
+
+export const carouselData: CarouselData[]  = [
+    {
+        title: "Samsung s22 ultra",
+        description: "Galaxy S21 Ultra 5G, epic in every way. Its camera revolutionizes photography and 8K video with 108MP resolution.",
+        image: phone2
+    },
+    {
+        title: "Dell xps 13",
+        description: "Dell XPS Plus 13 inch laptop featuring 12th Gen Intel Core processors with a modern design.",
+        image: laptop1
+    },
+    {
+        title: "BLEU CHANEL",
+        description: "BLEU DE CHANEL asserts an accomplished character through a timeless and unexpected scent.",
+        image: fragrance1
+    },
+    {
+        title: "Packed skincare products",
+        description: "Skin care is a range of practices that support skin integrity, enhance its appearance, and relieve skin conditions.",
+        image: skincare
+    },
+    {
+        title: "Best groceries this year",
+        description: "Save money on all your pantry staples and stock up your kitchen with the healthy groceries that will help you slim down",
+        image: groceries1
+    },
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,25 @@
-import Header from "../components/Layouts/Header";
 import React from "react";
 import {Product} from "../models/Product";
 import ProductList from "../components/Products/ProductList";
-import heroImage1 from '../public/images/hero_image_1.jpg';
-import heroImage2 from '../public/images/hero_image_2.jpg';
-import heroImage3 from '../public/images/hero_image_3.jpg';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import HeroProducts from "../components/Products/HeroProducts";
-import {Category} from "../models/Category";
+import {carouselData} from "../models/CarouselData";
 
 interface Props {
     smartphones: Product[],
     laptops: Product[],
-    categories: string[]
 }
 
-export default function Home({smartphones, laptops, categories}: Props) {
+export default function Home({smartphones, laptops}: Props) {
 
     return (
         <>
-            <HeroProducts images={[heroImage1, heroImage2, heroImage3]}/>
+            <HeroProducts carouselData={carouselData}/>
 
-            <ProductList products={smartphones} showViewAllButton={true} tittle="SmartPhones" description="best smartphonoes"/>
+            <ProductList products={smartphones} showViewAllButton={true} tittle="SmartPhones"
+                         description="best smartphones"/>
             <ProductList products={laptops} showViewAllButton={true} tittle="Laptops"
-                         description={"The best inhouse laptops right from the house"}/>
+                         description={"The best in-house laptops right from the house"}/>
         </>
     )
 }
@@ -32,13 +28,11 @@ export default function Home({smartphones, laptops, categories}: Props) {
 export async function getStaticProps() {
     const smartphones = await fetch("https://dummyjson.com/products/category/smartphones").then(res => res.json());
     const laptops = await fetch("https://dummyjson.com/products/category/laptops").then(res => res.json());
-    const categories = await fetch("https://dummyjson.com/products/categories").then(res => res.json());
 
     return {
         props: {
             smartphones: smartphones.products,
             laptops: laptops.products,
-            categories: categories,
         }, // will be passed to the page component as props
     }
 
