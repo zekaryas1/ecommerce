5a7104055921b7201d5c9138267d5585751d6d14
zekaryas <zekaryasdinku@gmail.com>
2022-12-07 12:26:08 +0300

    - on product click navigate to product detail page

--- components/Products/MainProduct.tsx
+++ components/Products/MainProduct.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import {Product} from "../../models/Product";
+import Link from "next/link";
 
 interface Props {
     product: Product
 }
 
 function MainProduct({product}: Props) {
-    return <div className="bg-gray-100 rounded p-3 hover:bg-gray-300 hover:cursor-pointer">
-        <Image className="w-full aspect-video object-cover " src={product.thumbnail} width="400" height="400" alt="product image"/>
-        <h2 className="font-bold">{ product.title }</h2>
-        <h2 className="truncate text-neutral-500">{ product.description }</h2>
-    </div>
+    return <Link href={`/shop/${product.id}`}>
+        <div className="bg-gray-100 rounded p-3 hover:bg-gray-300 hover:cursor-pointer">
+            <Image className="w-full aspect-video object-cover " src={product.thumbnail} width="400" height="400"
+                   alt="product image"/>
+            <h2 className="font-bold">{product.title}</h2>
+            <h2 className="truncate text-neutral-500">{product.description}</h2>
+        </div>
+    </Link>
 }
 
 export default MainProduct;
