2bf6dc3107000ac8a45983cb37d6348ad32ecff1
zekaryas <zekaryasdinku@gmail.com>
2022-12-09 16:09:29 +0300

    - progress indicator added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import type {AppProps} from 'next/app'
 import Header from "../components/Layouts/Header";
 import React from "react";
 import {categoryData} from "../models/Category";
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({Component, pageProps}: AppProps) {
     return <>
+        <NextNProgress color="#111827" />
         <Header categoryData={categoryData}/>
         <Component {...pageProps} />
     </>
