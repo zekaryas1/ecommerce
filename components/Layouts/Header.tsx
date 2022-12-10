cb437f4d169aca72001419845b767a837054e2f3
zekaryas <zekaryasdinku@gmail.com>
2022-12-10 12:09:34 +0300

    - header active state added

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import {HeartIcon, MagnifyingGlassCircleIcon, ShoppingBagIcon} from "@heroicons/react/24/outline";
+import {MagnifyingGlassCircleIcon, ShoppingBagIcon} from "@heroicons/react/24/outline";
 import logo from '../../public/images/logo.svg';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import {useRouter} from "next/router";
 import {CategoryData} from "../../models/Category";
-import Cart from "../Cart";
 
 interface Props {
     categoryData: CategoryData
@@ -15,6 +14,7 @@ interface Props {
 export default function Header({categoryData}: Props) {
     const router = useRouter();
     const optionalCategoryData = categoryData.others.map(value => value.name);
+    const currentCategory = router.query.category;
 
     const goTo = (destination: string) => {
         router.push(`/shop?category=${destination}`);
@@ -26,10 +26,13 @@ export default function Header({categoryData}: Props) {
             <Image className="fill-white h-6" src={logo} width={50} height={30} alt="website logo"/>
         </Link>
         <ul className="flex space-x-3 items-center">
+            <li><Link href='/shop' className={`uppercase hover:text-neutral-200 ${!currentCategory ? 'text-white font-bold' : 'text-neutral-500'} `}>Home</Link></li>
             {
+
                 categoryData.main.map((category) => {
                     return <li key={category.name}><Link href={`/shop?category=${category.name}`}
-                                                         className="uppercase">{category.name}</Link></li>
+                                                         className={`uppercase hover:text-neutral-200 ${currentCategory == category.name ? 'text-white font-bold' : 'text-neutral-500'} `}>{category.name}</Link>
+                    </li>
                 })
             }
             <Dropdown arrowClassName="arrowControl" controlClassName={"control"} options={optionalCategoryData}
