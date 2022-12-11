6938b43a327560f9898b73327e415e5fd16b2b76
zekaryas <zekaryasdinku@gmail.com>
2022-12-11 23:29:11 +0300

    - custom font added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,22 @@ import Header from "../components/Layouts/Header";
 import React from "react";
 import {categoryData} from "../models/Category";
 import NextNProgress from 'nextjs-progressbar';
+// import {Work_Sans} from '@next/font/google'
+import {Secular_One} from '@next/font/google'
+
+const inter = Secular_One({
+    subsets: ['latin'],
+    weight: '400'
+})
 
 export default function App({Component, pageProps}: AppProps) {
     return <>
-        <NextNProgress color="#111827" />
+        <style jsx global>{`
+          html {
+            font-family: ${inter.style.fontFamily};
+          }
+        `}</style>
+        <NextNProgress color="#111827"/>
         <Header categoryData={categoryData}/>
         <Component {...pageProps} />
     </>
